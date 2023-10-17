2689d0fa55994d1eed6c7ee8fba614bb4a561379
苟培烜 <3541037829@qq.com>
2023-10-17 17:08:28 +0800

    添加一个金子网站：稀土掘金

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,7 +2,7 @@
  * @Author: 苟培烜 3541037829@qq.com
  * @Date: 2023-10-12 21:59:30
  * @LastEditors: 苟培烜 3541037829@qq.com
- * @LastEditTime: 2023-10-17 16:56:28
+ * @LastEditTime: 2023-10-17 17:07:44
  * @FilePath: \temp-nav-site\src\pages\home.jsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -77,6 +77,12 @@ const oftenUsedSites: SiteBlockProps[] = [
     title: '腾讯云',
     description: '腾讯云',
     image: 'https://cloudcache.tencent-cloud.com/qcloud/favicon.ico',
+  },
+  {
+    url: 'https://juejin.cn/',
+    title: '掘金',
+    description: '掘金',
+    image: 'https://juejin.cn/favicon.ico',
   },
 ];
 
